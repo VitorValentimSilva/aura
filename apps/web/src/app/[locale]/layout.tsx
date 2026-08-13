834ef85de96158e0f1eb9eae9554af9fd4d93ff0
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-08-13 18:45:45 -0300

    style 💎: add min-h-full class to body element in layout.tsx
    The layout.tsx file in the web app has been updated to include a `min-h-full` class on the body element.

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -45,7 +45,7 @@ export default async function Layout({ children, params }: LayoutProps) {
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="flex min-h-full flex-col">
         <NextIntlClientProvider messages={messages}>
           <ThemeProvider
             attribute="class"
