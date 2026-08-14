697d7ebf5b33c3b4a81a0401c4fad4084c3c827c
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-08-14 19:50:22 -0300

    feat ✨: add new landing page component with header and main section
    Added a new landing page component to the web app, featuring a header and main section.

---
+++ apps/web/src/app/[locale]/(public)/page.tsx
@@ -0,0 +1,11 @@
+import { LandingHeader } from "@/components/landing/landing-header";
+
+export default function LandingPage() {
+  return (
+    <>
+      <LandingHeader />
+
+      <main className="container mx-auto flex max-w-7xl flex-col items-center justify-between px-5"></main>
+    </>
+  );
+}
