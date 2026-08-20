015eabaf52b81c28c1fa0d137cbc9ab2c0946e59
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-08-20 18:09:46 -0300

    fix 🐛: fixes image remote patterns in Next.js configuration.
    Updates the `next.config.ts` file to include remote patterns for Clerk images.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,11 +4,15 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "img.clerk.com" }],
+  },
+};
 
 export default withSentryConfig(withNextIntl(nextConfig), {
   org: "vitorvalem",
-  project: "aura-nextjs",
+  project: "aura-web",
   authToken: process.env.SENTRY_AUTH_TOKEN,
   silent: !process.env.CI,
   widenClientFileUpload: true,
