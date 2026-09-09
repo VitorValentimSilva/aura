eebd6db7c47e75bb5df1aa9c9e8c6f194329263b
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-09-09 20:32:10 -0300

    feat ✨: replace Clerk authentication with password-based authentication in User model

---
+++ packages/database/prisma/migrations/20260909230810_replace_clerk_with_password_auth/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `clerkId` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `firstName` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `imageUrl` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `lastName` on the `User` table. All the data in the column will be lost.
+  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "User_clerkId_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "clerkId",
+DROP COLUMN "firstName",
+DROP COLUMN "imageUrl",
+DROP COLUMN "lastName",
+ADD COLUMN     "name" TEXT NOT NULL,
+ADD COLUMN     "passwordHash" TEXT NOT NULL;
